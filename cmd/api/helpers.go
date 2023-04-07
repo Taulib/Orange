ed659c531cee410a0e5eb04b27ef59b74b7f101d
Taulib <2018118252@ub.edu.bz>
2023-04-07 16:47:01 -0600

    commit

---
+++ cmd/api/helpers.go
@@ -0,0 +1,77 @@
+// Filename: cmd/api/helpers.go
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type envelope map[string]interface{}
+
+func (app *application) readIDParams(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	// convert the data to a JSON format
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	// Add any headers that were sent
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	// add header information
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write([]byte(js))
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, destination interface{}) error {
+	// we will try to decode the JSON request
+	err := json.NewDecoder(r.Body).Decode(destination)
+	if err != nil {
+		// something went wrong
+		// let's find out the type of error
+		var syntaxError *json.SyntaxError
+		var unMarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+		// let's check for the type of decode error
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON(at character %d)",
+				syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.As(err, &unMarshalTypeError):
+			// which field has the wrong type
+			if unMarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q",
+					unMarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains badly-formed JSON(at character %d", unMarshalTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		default:
+			return err
+		}
+
+	}
+	return nil
+}
---
+++ cmd/api/schools.go
@@ -0,0 +1,59 @@
+// Filename: cmd/api/schools.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/taulib/orange/internal/data"
+)
+
+func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Request) {
+	// create a struct to hold a school that will be provided to us
+	// via the request
+	var input struct {
+		Name    string   `json:"name"`
+		Level   string   `json:"level"`
+		Contact string   `json:"contact"`
+		Phone   string   `json:"phone"`
+		Email   string   `json:"email"`
+		Website string   `json:"website,omitempty"`
+		Address string   `json:"address"`
+		Mode    []string `json:"mode"`
+	}
+	// decode our the JSON request
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Print the request
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParams(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	//fmt.Fprintf(w, "show details of school %d\n", id)
+	school := data.School{
+		ID:       id,
+		CreateAt: time.Now(),
+		Name:     "University of Belmopan",
+		Level:    "University",
+		Contact:  "Abel Blanco",
+		Phone:    "323-4545",
+		Website:  "https://uob.edu.bz",
+		Address:  "17 Apple Avenue",
+		Mode:     []string{"blended", "online", "face-to-face"},
+		Version:  1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"school": school}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
